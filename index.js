3004ce2d82feab33ce55cead8d4179ed9de9b2fc
nour khoury <khourynour79@gmail.com>
2022-09-14 22:59:07 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+const drivers = ['Rebecca','Nour','Tony','georges'];
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiper = function(fraeMultiper){
+    return function(value){
+        return fareMultipler * value;
+    }
+}
+
+const fareDoubler = createFareMultiper(2);
+const fareTripler = createFareMultiper(3);
+
+function fetchSpeecifiedDrivers(drivers,func){
+    return func(drivers);
+}
+
+
